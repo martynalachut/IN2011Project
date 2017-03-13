1c1915ccfd8bd0ef7cbd7a58dbc5da1b4df1a8be
Martyna Lachut <martyna.lachut@gmail.com>
2017-03-13 15:25:35 +0000

    Start-up

---
+++ WebServer/src/webserver/WebServer.java
@@ -0,0 +1,71 @@
+package webserver;
+
+import in2011.http.Request;
+import in2011.http.Response;
+import in2011.http.StatusCodes;
+import in2011.http.EmptyMessageException;
+import in2011.http.MessageFormatException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.*;
+import java.nio.*;
+import java.util.Date;
+import org.apache.http.client.utils.DateUtils;
+
+public class WebServer {
+
+    private int port;
+    private String rootDir;
+
+    public WebServer(int port, String rootDir) {
+        this.port = port;
+        this.rootDir = rootDir;
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        while (true)
+        {
+            Socket con = serverSocket.accept();
+            InputStream is = con.getInputStream();
+            
+            try
+            {
+                Request req = Request.parse(is);
+            } catch (MessageFormatException ex)
+            {          
+            
+            }
+            
+            OutputStream os = con.getOutputStream();
+            
+            Response msg = new Response(200);
+            msg.write(os);
+            os.write("Hello World \n".getBytes());
+            
+            con.close();
+        }
+    }
+    
+    public void getMethod()
+    {
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        String usage = "Usage: java webserver.WebServer <port-number> <root-dir>";
+        if (args.length != 2) {
+            throw new Error(usage);
+        }
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            throw new Error(usage + "\n" + "<port-number> must be an integer");
+        }
+        String rootDir = args[1];
+        WebServer server = new WebServer(port, rootDir);
+        server.start();
+    }
+}
